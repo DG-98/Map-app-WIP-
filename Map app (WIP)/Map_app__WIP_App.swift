c2bf1c3c1b2d190aeb112d095bfe90d9a545420d
Derrick Garcon <dgarconpro@gmail.com>
2023-04-07 12:09:35 -0400

    Initial Commit

---
+++ Map app (WIP)/Map_app__WIP_App.swift
@@ -0,0 +1,17 @@
+//
+//  Map_app__WIP_App.swift
+//  Map app (WIP)
+//
+//  Created by Derrick on 4/7/23.
+//
+
+import SwiftUI
+
+@main
+struct Map_app__WIP_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
